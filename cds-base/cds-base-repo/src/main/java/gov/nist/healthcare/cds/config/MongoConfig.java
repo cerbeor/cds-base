da414b3910ed823f03779c5ef7f59b7635481b11
Hossam Tamri <hossam.tamri@nist.gov>
2024-08-22 10:44:21 -0400

    Add mongo credentials config

--- cds-base/cds-base-repo/src/main/java/gov/nist/healthcare/cds/config/MongoConfig.java
+++ cds-base/cds-base-repo/src/main/java/gov/nist/healthcare/cds/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package gov.nist.healthcare.cds.config;
 
+import com.mongodb.MongoCredential;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -9,27 +10,49 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
+import java.net.UnknownHostException;
+import java.util.Collections;
+
 @Configuration
 @EnableMongoRepositories(value="gov.nist.healthcare.cds")
 public class MongoConfig extends AbstractMongoConfiguration {
 
 	@Value("${fits.data.mongodb.host}")
-	private String host;
+	private String HOST;
 
 	@Value("${fits.data.mongodb.port}")
-	private String port;
+	private String PORT;
 
 	@Value("${fits.data.mongodb.database}")
-	private String db;
+	private String NAME;
+
+	@Value("${fits.data.mongodb.username}")
+	private String USERNAME;
+
+	@Value("${fits.data.mongodb.password}")
+	private String PASSWORD;
+
+	@Value("${fits.data.mongodb.auth.source}")
+	private String AUTH_SOURCE;
 
 	@Override
 	protected String getDatabaseName() {
-		return db;
+		return NAME;
 	}
 
 	@Override
-	public Mongo mongo() throws Exception {
-		return new MongoClient(new ServerAddress(host, Integer.parseInt(port)));
+	public Mongo mongo() throws UnknownHostException {
+		if(USERNAME != null && PASSWORD != null && !USERNAME.isEmpty() && !PASSWORD.isEmpty()) {
+			MongoCredential credential = MongoCredential.createPlainCredential(USERNAME, AUTH_SOURCE, PASSWORD.toCharArray());
+			return new MongoClient(
+					new ServerAddress(
+							HOST,
+							Integer.parseInt(PORT)
+					),
+					Collections.singletonList(credential)
+			);
+		}
+		return new MongoClient(new ServerAddress(HOST, Integer.parseInt(PORT)));
 	}
 
 	@Override
